bb2d860e500c26f87c5a507bff8a8de5363f98fe
Doeke <doekedewolf@gmail.com>
2024-08-16 12:02:22 +0200

    repro for bug

---
+++ vue-project/src/components/TextInput.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineProps<{
+  modelValue?: string | null
+}>()
+
+const model = defineModel<string | null>()
+</script>
+
+<template>
+  <input
+    v-model="model"
+  />
+</template>
